3660a0ccb5d2f8776d96a94872f9c0219e3ffe52
Caleb Brehaut <githubuser@brehautsolutions.com>
2019-01-13 20:11:33 +0000

    Initial Check-in
    Initial check in containing data access test

---
+++ BlobStorageTest/Settings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobStorageTest
+{
+    public class Settings
+    {
+        public string StorageConnectionString {get; set;}
+        public string StorageContainer { get; set; }
+        public string CdnConnectionString { get; set; }
+
+        public string StaticContentBaseUrl
+        {
+            get
+            {
+                var account = CloudStorageAccount.Parse(StorageConnectionString);
+                return string.Format("{0}/{1}", account.BlobEndpoint.ToString().TrimEnd('/'), StorageContainer.TrimStart('/'));
+            }
+        }
+    }
+}
---
+++ BlobStorageTest/StaticContentUrlHtmlHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace BlobStorageTest
+{
+    public static class StaticContentUrlHtmlHelper
+    {
+        public static string StaticContentUrl(this IHtmlHelper<dynamic> helper, Settings settings, string contentPath)
+        {
+            if (contentPath.StartsWith("~"))
+            {
+                contentPath = contentPath.Substring(1);
+            }
+
+            contentPath = string.Format("{0}/{1}", settings.StaticContentBaseUrl.TrimEnd('/'), contentPath.TrimStart('/'));
+
+            var url = new UrlHelper(helper.ViewContext);
+
+            return url.Content(contentPath);
+        }
+
+        public static string CdnUrl(this IHtmlHelper<dynamic> helper, Settings settings, string contentPath)
+        {
+            if (contentPath.StartsWith("~"))
+            {
+                contentPath = contentPath.Substring(1);
+            }
+
+            contentPath = string.Format("{0}/{1}", settings.CdnConnectionString.TrimEnd('/'), contentPath.TrimStart('/'));
+
+            var url = new UrlHelper(helper.ViewContext);
+
+            return url.Content(contentPath);
+        }
+    }
+}
